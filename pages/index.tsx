c5f28093614e47953fb0cf7083f168b23fb0ed3f
Sangjun-man <molung@icloud.com>
2022-10-24 22:54:35 +0900

    add : interaction page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Main from '../components/home/Main';
+// import Main from '../components/home/Main';
 import MainNav from '../components/layouts/HomeLayout/MainNav';
 import ScrollTop from '../components/layouts/HomeLayout/ScrollTop';
 import Head from 'next/head';
+import Main from '../components/intro/Main';
 
 const MainPage = () => {
   return (
